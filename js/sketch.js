302bf81b7d52ca12a331326c77704724d2df401e
Robbo270 <116882868+Robbo270@users.noreply.github.com>
2022-11-18 12:19:17 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,3 +1,6 @@
+let words = ["Plymouth", "is", "awesome"];
+let wordIndex = 0;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
@@ -5,7 +8,16 @@ function setup() {
 
 function draw() {
     background(220);
+    textSize(mouseX);
+    text(words[wordIndex], mouseX, mouseY);
+}
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+function mousePressed(){
+    if (wordIndex == words.length-1){
+        wordIndex = 0
+    } else {
+        wordIndex++;
+    }
+
+    console.log('wordIndex: '+wordIndex);
 }
